e1926a33f7769640846bf670a59d892d46df7e27
tony306139 <tony306139@gmail.com>
2019-02-01 13:42:35 +0800

    Add a new GUI test

---
+++ GUI_main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+#========================================
+root = tk.Tk()
+root.title("GUI Test")
+root.resizable(width=False, height=False)
+root.geometry("400x300")
+#========================================
+
+#========================================
+root.mainloop()
